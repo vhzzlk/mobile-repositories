9f4793cc498a0c7c6bf719abdc3338d030788c9a
vhzzlk <vhzzlk@gmail.com>
2026-03-26 11:10:41 -0300

    feat: add todo-list

---
+++ todo-list/src/constants/styles.js
@@ -0,0 +1,24 @@
+import { Plataform, StatusBar } from 'react-native';
+
+export const prioridades = {
+    alta: '#f00',
+    media: '#ff9800',
+    baixa: '#00b800',
+};
+
+export const STATUS_BAR_HEIGHT = Plataform.OS === 'android' ? StatusBar.currentHeight : 0;
+export const PRIMARY_COLOR = '#6200ee';
+export const ACCENT_COLOR_YELLOW = '#ffc107';
+export const ACCENT_COLOR_RED = '#f44336';
+
+export const TEXT_COLOR_DARK = '#333';
+export const TEXT_COLOR_LIGHT = '#fff';
+export const TEXT_COLOR_MEDIUM = '#555';
+export const TEXT_COLOR_DISABLED = '#aaa';
+
+export const BACKGROUND_COLOR_LIGHT = '#f5f5f5';
+export const BACKGROUND_COLOR_WHITE = '#fff';
+
+export const BORDER_COLOR_LIGHT = '#ccc';
+export const BACKGROUND_COLOR_INPUT = '#f9f9f9';
+export const OVERLAY_COLOR = 'rgba(0, 0, 0, 0.6)'; 
---
+++ todo-list/src/styles/TaskItemStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import { PRIMARY_COLOR, ACCENT_COLOR_YELLOW, ACCENT_COLOR_RED, TEXT_COLOR_DARK, TEXT_COLOR_DISABLED, BACKGROUND_COLOR_WHITE } from "../constants/styles";
+
+export default Styles = StyleSheet.create({
+    taskItem: {
+        backgroundColor: BACKGROUND_COLOR_WHITE,
+        paddingVertical: 12,
+        paddingHorizontal: 15,
+    },
+});
