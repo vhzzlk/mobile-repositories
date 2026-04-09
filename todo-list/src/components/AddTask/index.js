e68a746fecdf206cf56b79b23429f601828f7f43
vhzzlk <vhzzlk@gmail.com>
2026-04-09 10:58:46 -0300

    eu continuo achando isso certo

--- todo-list/src/components/AddTask/index.js
+++ todo-list/src/components/AddTask/index.js
@@ -1,67 +1,79 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Modal, View, Text, TextInput, TouchableOpacity } from "react-native";
 import style from "./style.js";
 
-export default function TaskModal({ visible, onClose, onSave, taskToEdit }) {
-    const [task, setTask] = useState(taskToEdit || { title: "", description: "" });
+export default function AddTask({ visible, onClose, onSave, taskToEdit }) {
+  const [task, setTask] = useState(
+    taskToEdit || { title: "", description: "" },
+  );
 
-    const HandleSave = () => {
-        onSave(task);
-        setTask({ title: "", description: "" , priority: "", star: "", end: ""});
-    };
-    return (
-        <Modal visible={visible} animationType="slide">
-            <View style={style.modal}>
-                <Text style={style.titulo}>
-                    Nova tarefa
-                </Text>
-                <TextInput
-                    placeholder="Titulo"
-                    style={style.input}
-                    onChangeText = {t => setTask({
-                        ...task, title: t
-                    })}
-                />
-                <TextInput
-                    placeholder="Descrição"
-                    style={style.input}
-                    onChangeText = {d => setTask({
-                        ...task, description: d
-                    })}
-                />
-                <TextInput
-                    placeholder="Prioridade baixa, média ou alta"
-                    style={style.input}
-                    onChangeText = {p => setTask({
-                        ...task, priority: p
-                    })}
-                />
-                <TextInput
-                    placeholder="Data de início: DD/MM"
-                    style={style.input} 
-                    onChangeText = {s => setTask({
-                        ...task, start: s
-                    })}
-                />
-                   <TextInput
-                    placeholder="Data de fim"
-                    style={style.input} 
-                    onChangeText = {e => setTask({
-                        ...task, end: e
-                    })}
-                />
+  const HandleSave = () => {
+    onSave(task);
+    setTask({ title: "", description: "", priority: "", start: "", end: "" });
+  };
+  return (
+    <Modal visible={visible} animationType="slide">
+      <View style={style.modal}>
+        <Text style={style.titulo}>Nova tarefa</Text>
+        <TextInput
+          placeholder="Titulo"
+          style={style.input}
+          onChangeText={(t) =>
+            setTask({
+              ...task,
+              title: t,
+            })
+          }
+        />
+        <TextInput
+          placeholder="Descrição"
+          style={style.input}
+          onChangeText={(d) =>
+            setTask({
+              ...task,
+              description: d,
+            })
+          }
+        />
+        <TextInput
+          placeholder="Prioridade baixa, média ou alta"
+          style={style.input}
+          onChangeText={(p) =>
+            setTask({
+              ...task,
+              priority: p,
+            })
+          }
+        />
+        <TextInput
+          placeholder="Data de início: DD/MM"
+          style={style.input}
+          onChangeText={(s) =>
+            setTask({
+              ...task,
+              start: s,
+            })
+          }
+        />
+        <TextInput
+          placeholder="Data de fim"
+          style={style.input}
+          onChangeText={(e) =>
+            setTask({
+              ...task,
+              end: e,
+            })
+          }
+        />
 
-                <TouchableOpacity style={style.btnSalvar} onPress={HandleSave}>
-                    <Text>Salvar Tarefa</Text>
-                </TouchableOpacity>
+        <TouchableOpacity style={style.btnSalvar} onPress={HandleSave}>
+          <Text>Salvar Tarefa</Text>
+        </TouchableOpacity>
 
-                <TouchableOpacity onPress={onClose}>
-                    <Text style={style.txtCancelar}>Cancelar</Text>
-                </TouchableOpacity>
-            </View>
-        </Modal>
-        )   ;
+        <TouchableOpacity onPress={onClose}>
+          <Text style={style.txtCancelar}>Cancelar</Text>
+        </TouchableOpacity>
+      </View>
+    </Modal>
+  );
 }
-    
-
-
--- todo-list/src/components/AddTask/style.js
+++ todo-list/src/components/AddTask/style.js
@@ -2,30 +2,28 @@ import { StyleSheet } from "react-native";
 import { theme } from "../../styles/global";
 
 export const styles = StyleSheet.create({
-    modal: {
-            flex: 1,
-            backgroundColor: theme.colors.background,
-            padding: 30,
-    },
-    titulo: {
-        fontSize: 20,
-        fontWeight: "bold",
-        marginBottom: 20,
-    },
-    input: {
-        backgroundColor: "#fff",
-        padding: 15, 
-        borderRadius: 8,
-        marginBottom: 10,
-        borderWidth: 1,
-        borderColor: "#ddd",
-    },
-    btnSalvar: {
-        backgroundColor: theme.colors.primary,
-        padding: 15,
-        borderRadius: 10,
-        alignItems: "center",
-    }
-
-    
-    })
+  modal: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    padding: 30,
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 8,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  btnSalvar: {
+    backgroundColor: theme.colors.primary,
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+});
